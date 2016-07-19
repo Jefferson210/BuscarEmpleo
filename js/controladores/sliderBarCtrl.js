6ab3ce527173793364de4c80bcca16e9225181cd
Jefferson <morochoji5@hotmail.com>
2016-07-19 15:00:41 -0500

    creacion de formulario en el index

--- js/controladores/sliderBarCtrl.js
+++ js/controladores/sliderBarCtrl.js
@@ -1,5 +1,25 @@
 app.controller("sliderBarCtrl",["$scope",function($scope){
-    $scope.demo6 = {
-    valueA : 100
-};
+    $scope.educacion = {
+        max_value : 100
+    };
+    
+    $scope.exp_laboral = {
+        max_value : 100
+    };
+    
+    $scope.min_edad = [];
+    for(i = 20; i<=50;i++){
+        $scope.min_edad.push(i);
+    }
+    
+    $scope.max_edad = [];
+    for(i = 20; i<=50;i++){
+        $scope.max_edad.push(i);
+    }
+    
+    $scope.ubicacion = ["Azuay","Bolivar" ,"Cañar","Carchi","Chimborazo","Cotopaxi","El Oro","Esmeraldas",
+    "Galápagos","Guayas","Imbabura","Loja","Los Rios","Manabí","Morona Santiago","Napo","Orellana","Pastaza", 
+    "Pichincha","Santa Elena","Sto.Domingo Tsáchilas","Sucumbíos","Tungurahua","Zamora Chinchipe"];
+
+    
 }]);
